017a1543f5b830b499cfb0db9df17b7f15aaacee
zhuchenyu2008 <132262479+zhuchenyu2008@users.noreply.github.com>
2026-08-10 09:12:43 +0800

    build: compile bridge against Android API 36

--- oppo-health-bridge/app/build.gradle.kts
+++ oppo-health-bridge/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.zhuchenyu.oppohealthbridge"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.zhuchenyu.oppohealthbridge"
